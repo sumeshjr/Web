edbdb0849853e39ae958d0725412da4c1ff5642a
unknown <sumeshjohn789@gmail.com>
2023-06-01 19:36:34 +0530

    Add All My Files

---
+++ streamproject/streamapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Category)
+admin.site.register(Video)
+admin.site.register(Comment)
+admin.site.register(RecentUpdate)
+admin.site.register(Home)
+admin.site.register(UserReg)
+admin.site.register(ContactUs)
+# admin.site.register(View)
+# Register your models here.
